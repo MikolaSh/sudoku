9a95dd4f8aa53a01e790dd52058709e0217c52d8
MikolaSh <nikolashulga2000@gmail.com>
2021-09-06 23:16:14 +0300

    feat: my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,79 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+    const size = 9;
+    const boxSize =3;
+    
+    const findEmpty = (matrix) => {
+        for (let i = 0; i < size; i++) {
+            for (let j = 0; j < size; j++) {
+                if(matrix[i][j]==0){
+                    return [i,j];
+                }
+            }
+        }
+        return null;
+    }
+
+    const validate = (num, position, matrix)=>{
+        const [r,c] = position;
+
+        //rows
+        for(let i = 0; i<size;i++){
+            if(matrix[i][c]=== num && i!==r){
+                return false;
+            }
+        }
+
+        //cols
+        for(let i = 0; i<size;i++){
+            if(matrix[r][i]=== num && i!==c){
+                return false;
+            }
+        }
+
+        //box
+        const boxRow = Math.floor(r/boxSize) * boxSize
+        const boxCol = Math.floor(c/boxSize) * boxSize
+
+        for(let i = boxRow;i<boxRow+boxSize;i++){
+            for (let j = boxCol; j < boxCol + boxSize; j++) {
+                if(matrix[i][j] == num && i!==r && j!==c){
+                    return false;
+                }
+                
+            }
+        }
+
+        return true;
+
+    }
+
+    const solve = () => {
+        const currPosition = findEmpty(matrix);
+
+        if(currPosition === null){
+            return true;
+        }
+
+        for (let i = 1; i < size+1; i++) {
+            const currNumber = i;
+            const isValid = validate(currNumber, currPosition, matrix)
+
+            if(isValid){
+                const [x,y] = currPosition;
+                matrix[x][y] = currNumber;
+
+                if(solve()){
+                    return true;
+                }
+
+                matrix[x][y] = 0;
+            }
+        }
+
+        return false;
+    }
+
+
+    solve();
+    return matrix;
 }
